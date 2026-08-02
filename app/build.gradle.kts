8c95ab3498eaacdf4acd6bd0d696713c6970ed4c
miguel-dev19 <tu-email@github.com>
2026-08-02 10:27:35 -0400

    Ajustar versión a 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.seend.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     buildTypes {
